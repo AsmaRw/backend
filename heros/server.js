db66d0afc9005b9cc730dbcd68fd8c0f7e402190
AsmaRW <asma.rw09@gmail.com>
2021-05-31 17:32:05 +0200

    nn

--- heros/server.js
+++ heros/server.js
@@ -24,16 +24,47 @@ app.get("/heros", (req, res) => {
 app.get("/heros/:herosName", (req, res) => {
     const herosName = req.params.herosName
     console.log('herosname', herosName);
-    
-    const dataHeros = heros.find( elem => {
+
+    const dataHeros = heros.find(elem => {
         console.log('elem', elem);
-        
+
         return elem.name.toLocaleLowerCase() === herosName.toLocaleLowerCase()
     })
-    
+
     console.log('data', dataHeros);
     res.json(dataHeros)
 })
+app.get("/heros/:herosName/powers", (req, res) => {
+    const herosName = req.params.herosName
+
+    const dataPower = heros.find(elem => {
+
+        return elem.name.toLocaleLowerCase() === herosName.toLocaleLowerCase()
+    })
+    // console.log('power', dataPower);
+    if (herosName) {
+
+        res.json({
+            power : dataPower.power
+        })
+    }
+
+})
+
+
+
+app.post("/heros", (req,res)=> {
+    const newHeros = req.body
+
+    heros.push(newHeros)
+
+    res.json({
+        message: 'heros added !'
+    })
+})
+
+
+
 
 app.listen(port, () => {
     console.log("Server à l'écoute dans le port " + port);
