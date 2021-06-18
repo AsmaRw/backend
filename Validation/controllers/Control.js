bd441c34c8f118a96edfe7ce4ac28b1180a5a857
AsmaRW <asma.rw09@gmail.com>
2021-06-18 14:27:32 +0200

    new

--- Validation/controllers/Control.js
+++ Validation/controllers/Control.js
@@ -4,17 +4,23 @@ const City = require("../models/user");
 
 const newRoute = async (req, res) => {
     try {
-        const errors = expressValidator.validationResult(req);
-        const people = req.body
+        const error = expressValidator.validationResult(req);
+        const user = req.body
 
+        if (!error.isEmpty()) {
+            res.status(400).json({ message: "error from the user" });
+        } else {
 
-        ///
+            const cityN = await City.findOne({ name: user.city })
+            user.city = cityN
 
-        ////
-
+            await User.create(user)
+            res.json({ message: 'add a new user' })
+        }
     } catch (error) {
-        res.status(500).json({ errorMessage: "err 500" })
+        res.status(500).json({ errorMessage: "error from the server" })
     }
 }
+
 
 module.exports = { newRoute }
