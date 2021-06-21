ff2a4e85a56130d2f8014b24112f9aba5f4b134d
AsmaRW <asma.rw09@gmail.com>
2021-06-21 12:18:51 +0200

    back

--- Validation/controllers/Control.js
+++ Validation/controllers/Control.js
@@ -1,13 +1,13 @@
-// const expressValidator = require("express-validator");
+const expressValidator = require("express-validator");
 
 
-// const newRoute = async (req, res) => {
-//     try {
-//         const error = expressValidator.validationResult(req);
-//         const yuser = req.body
-//     } catch (error) {
-//         res.status(500).json({ errorMessage: "There was a problem !!!" })
-//     }
-// }
+const newRoute = async (req, res) => {
+    try {
+        const error = expressValidator.validationResult(req);
+        const yuser = req.body
+    } catch (error) {
+        res.status(500).json({ errorMessage: "There was a problem !!!" })
+    }
+}
 
-// module.exports = { newRoute }
+module.exports = { newRoute }
