68173cd674e1d9d13c1150a4b1a7c3260f5f116f
AsmaRW <asma.rw09@gmail.com>
2021-06-22 15:18:27 +0200

    d commit

--- chap06/server/server.js
+++ chap06/server/server.js
@@ -1,24 +1,33 @@
 const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { debug } = require("./middleware/debug")
+const { utilisateurs } = require("./Model/utilisateurs")
 
-mongoose.connect("mongodb://localhost:27017/auth",{
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
 
-const port = 8111
-
+/*{app config}*/
 const app = express()
+const port = process.env.Port || 8187
+
 
 //middleware
 app.use(cors())
 app.use(express.json())
 app.use(debug)
 
-///
+//db config
+mongoose.connect("mongodb://localhost:27017/auth",{
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+// routes
+
+
+
 
 
 //listen
