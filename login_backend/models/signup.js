e8a38a682da690aa27768d5d73f5016fa336c0b6
AsmaRW <asma.rw09@gmail.com>
2021-06-23 17:20:15 +0200

    hash

--- login_backend/models/signup.js
+++ login_backend/models/signup.js
@@ -1,16 +1,33 @@
 const mongoose = require("mongoose")
 const uniqueValidator = require('mongoose-unique-validator');
 const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
 
 const signupSchema = new mongoose.Schema({
-    username: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
+    username: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true },
     email: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true },
     password: String,
     role: Number
-}, {timestamps: true})
+}, { timestamps: true })
 
-signupSchema.plugin(uniqueValidator, {message: 'is already taken.'});
+signupSchema.plugin(uniqueValidator, { message: 'is already taken.' });
 
 const signupMode = mongoose.model("login", signupSchema)
+
+
+
+signupSchema.methods.setPassword = function (password) {
+    his.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+};
+
+signupSchema.methods.validPassword = function (password) {
+    const hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+    return this.hash === hash;
+};
+
+
+
+
 
 module.exports = signupMode
