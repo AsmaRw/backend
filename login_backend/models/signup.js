a7484131c4906bc341b6705a7151e35bbd00dc75
AsmaRW <asma.rw09@gmail.com>
2021-06-23 16:10:43 +0200

    hash

--- login_backend/models/signup.js
+++ login_backend/models/signup.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose")
-var uniqueValidator = require('mongoose-unique-validator');
+const uniqueValidator = require('mongoose-unique-validator');
+const crypto = require('crypto');
+
 const signupSchema = new mongoose.Schema({
     username: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
     email: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true },
