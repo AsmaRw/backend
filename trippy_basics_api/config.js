4c68964bf4ad5f2cc8a529891a4ad40ec9ea7060
AsmaRW <asma.rw09@gmail.com>
2021-06-24 17:18:29 +0200

    back

---
+++ trippy_basics_api/config.js
@@ -0,0 +1,7 @@
+const config = {
+    secret: "monmotsecretHERO",
+    port: 8989,
+    mongoURL: "mongodb://localhost:27017/trip"
+}
+
+module.exports = config
--- trippy_basics_api/server.js
+++ trippy_basics_api/server.js
@@ -2,20 +2,25 @@ const express = require("express")
 const mongoose = require("mongoose")
 const hotelRoutes = require("./routes/hotelRoutes")
 const restaurantRoutes = require("./routes/restaurantRoutes")
+const { port, mongoURL } = require('./config.js')
 
-mongoose.connect("mongodb://localhost:27017/trip", { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+
+mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
     if (err) {
-        console.log("There was a problem when connection to the database")
+        console.error(err);
     } else {
         console.log("I'm connected to the database")
     }
 })
 
-const port = 9001
 
 const app = express()
 
+app.use(cors())
+
 app.use(express.json())
+
+app.use(debug)
 
 app.use("/hotels", hotelRoutes)
 app.use("/restaurants", restaurantRoutes)
